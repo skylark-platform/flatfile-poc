a2288890c7a9dbabd16ee7413ee17ed6744364df
James Wallis <j@wallis.dev>
2022-10-28 12:07:46 +0100

    refactor: update variable name

--- constants.ts
+++ constants.ts
@@ -1,5 +1,5 @@
 export const FLATFILE_GRAPHQL_URL="https://api.us.flatfile.io/graphql"
-export const FLATFILE_TEAM = process.env.NEXT_PUBLIC_FLATFILE_TEAM_ID as string;
+export const FLATFILE_TEAM = process.env.FLATFILE_TEAM_ID as string;
 export const FLATFILE_ENV = {
   PROD: "64c7b20f-9d95-41ba-a582-e811f37dc619",
   TEST: "d3b7a781-b741-4b82-b028-10cc03321f85"
