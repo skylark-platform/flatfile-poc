0c33a9b7b1d334aff92a57bd792db4c5b317328d
James Wallis <j@wallis.dev>
2022-11-10 19:01:18 +0000

    feat: call import to skylark endpoint onComplete

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,8 @@
-import "../styles/globals.css";
 import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import "@fontsource/work-sans/700.css";
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/600.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
